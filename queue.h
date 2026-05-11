f21419ac8d02af317e82a98e0caa17cdeca66caa
Barok Yeshiber <barokyeshibr@gmail.com>
2026-05-11 19:20:16 +0300

    Remove stdio.h include from queue.h
    Removed unnecessary include for stdio.h.

--- queue.h
+++ queue.h
@@ -1,7 +1,6 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-#include <stdio.h>
 #include <stdbool.h>
 
 #define MAX_PROCESSES 100
@@ -13,6 +12,7 @@ typedef struct {
     int count;
 } Queue;
 
+// Function Prototypes (The Contract)
 void initQueue(Queue* q);
 bool isEmpty(Queue* q);
 void enqueue(Queue* q, int process_index);
